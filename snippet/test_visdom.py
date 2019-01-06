2d473100d8943d0e2566f8c4601cee9a34efa93e
KAI ZHAO <kaiz.xyz@gmail.com>
2019-01-07 00:54:35 +0800

    test visdom

--- snippet/test_visdom.py
+++ snippet/test_visdom.py
@@ -1,8 +1,20 @@
 import visdom
 import numpy as np
 
-vis = visdom.Visdom(port=8100)
+vis = visdom.Visdom(port=8098)
 
-y = np.random.rand(10, 2)
+x = np.array([np.arange(0, 10, 0.01), np.arange(0, 10, 0.01)]).transpose()
 
-vis.line(y, opts=dict({"legend": ["a", "b"], "title": "aa", "fillarea": True}))
+y = np.sin(x)
+y[:, 1] = np.cos(x[:, 1])
+
+vis.line(y, x,
+    opts=dict({
+        "title": "aa",
+        "fillarea": True,
+        "ytickmax": 1,
+        "ytickmin": -1,
+        "xtickmax": 9,
+        "xtickmin": 0,
+        "xtickstep":
+1}), win=4)
