0556e4ea99d46a146b19c64c650b593b14c4597d
KAI ZHAO <kaiz.xyz@gmail.com>
2019-02-19 12:58:08 +0800

    prelu

---
+++ classification/models/prelu.py
@@ -0,0 +1,23 @@
+
+import torch
+import torch.nn as nn
+
+class PReLU(nn.Module):
+
+    def __init__(self, planes):
+        super(PReLU, self).__init__()
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.pos_splope = nn.Conv2d(in_channels=planes, out_channels=planes, kernel_size=1,
+                                groups=planes, bias=False)
+        self.bn = nn.BatchNorm2d(planes)
+
+    def forward(self, x):
+
+        w = self.bn(self.pos_splope(self.avgpool(x)))
+        w = torch.sigmoid(w)
+        w = w.expand_as(x)
+
+        y = x * w
+        y[x < 0] *= 0
+
+        return y
