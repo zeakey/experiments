f1e6d383dbe2973c16169fe928034a5b69218d10
KAI ZHAO <kaiz.xyz@gmail.com>
2019-02-19 22:43:13 +0800

    prelu

--- classification/models/prelu.py
+++ classification/models/prelu.py
@@ -2,10 +2,22 @@
 import torch
 import torch.nn as nn
 
-class PReLU(nn.Module):
+
+class My_ReLU(nn.Module):
 
     def __init__(self, planes, neg_slope=0.25, reduction=4):
-        super(PReLU, self).__init__()
+        super(My_ReLU, self).__init__()
+
+    def forward(self, x):
+
+        #x[x < 0] *= 0
+
+        return x
+
+class PReLU_fc(nn.Module):
+
+    def __init__(self, planes, neg_slope=0.25, reduction=4):
+        super(PReLU_fc, self).__init__()
         self.neg_slope = neg_slope
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
 
@@ -41,6 +53,37 @@ class PReLU(nn.Module):
         y[x < 0] = x[x < 0] * w1[x < 0]
 
         return y
+
+class PReLU_conv1x1(nn.Module):
+
+    def __init__(self, planes, neg_slope=0.25, reduction=4):
+        super(PReLU_conv1x1, self).__init__()
+        self.neg_slope = neg_slope
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.fc0 = nn.Sequential(
+            nn.Conv2d(planes, planes, groups=planes, kernel_size=1, bias=False),
+            nn.Sigmoid()
+        )
+
+        self.fc1 = nn.Sequential(
+            nn.Conv2d(planes, planes, groups=planes, kernel_size=1, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+
+        b, c, _, _ = x.size()
+        w = self.avgpool(x)
+
+        w0 = self.fc0(w).view(b, c, 1, 1).expand_as(x)
+        w1 = self.fc1(w).view(b, c, 1, 1).expand_as(x)
+
+        w1 = w1 * self.neg_slope
+
+        w0[x < 0] *= 0
+
+        return x
 
 class SELayer(nn.Module):
     def __init__(self, channel, reduction=4):
