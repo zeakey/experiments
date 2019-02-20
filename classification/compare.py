a9412bc47d2b877792fdca2ecfd15ea2f6135484
KAI ZHAO <kaiz.xyz@gmail.com>
2019-02-20 14:01:53 +0800

    compare

--- classification/compare.py
+++ classification/compare.py
@@ -1,15 +1,25 @@
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
-mats = ["tmp/cifar100-resnet_se_cifar.resnet18/record.mat",
-        "tmp/cifar100-resnet_prelufc_cifar.resnet18/record.mat",
-        "tmp/cifar100-resnet_cifar.resnet18/record.mat"]
+mats = ["tmp/cifar100-resnet_se_cifar.resnet34/record.mat",
+        "tmp/cifar100-resnet_prelufc_cifar.resnet34/record.mat",
+        "tmp/cifar100-resnet_cifar.resnet34/record.mat"]
 
+legends = ["SE-ResNet", "PReLU-fc", "ResNet"]
 
-
+fig, axes = plt.subplots(1, 2, figsize=(8, 4))
 for i in range(len(mats)):
     mats[i] = loadmat(mats[i])
-    print(mats[i]["acc1"].squeeze().shape)
-    plt.plot(mats[i]["acc1"].squeeze())
-plt.grid(alpha=0.5, linestyle='dotted', linewidth=2, color='black')
-plt.legend(["SE-ResNet", "PReLU-fc", "ResNet"])
-plt.savefig("se-vs-prelufc-vs-resnet18.pdf")
+    axes[0].plot(mats[i]["acc1"].squeeze())
+    axes[0].plot(mats[i]["acc5"].squeeze())
+    axes[0].set_title("Acc")
+
+    axes[1].plot(mats[i]["loss_record"].squeeze())
+    axes[1].set_title("Loss")
+
+
+for ax in axes:
+    ax.grid(alpha=0.5, linestyle='dotted', linewidth=2, color='black')
+    ax.legend(legends)
+
+# plt.legend(["SE-ResNet", "PReLU-fc", "ResNet"])
+plt.savefig("se-vs-prelufc-vs-resnet34.pdf")
