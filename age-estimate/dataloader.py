bdf2319a9013ce3d324a5190cfb6f451572d50f6
KAI ZHAO <kaiz.xyz@gmail.com>
2019-03-25 21:03:27 +0800

    age-dataloader: imdb

--- age-estimate/dataloader.py
+++ age-estimate/dataloader.py
@@ -2,6 +2,8 @@ import torch
 import torch.utils.data as data
 from torchvision.datasets.folder import pil_loader
 from os.path import join, split, splitext, abspath, dirname, isfile, isdir
+from scipy.io import loadmat
+from datetime import datetime, timedelta
 
 class CACDDataset(torch.utils.data.Dataset):
 
@@ -23,10 +25,52 @@ class CACDDataset(torch.utils.data.Dataset):
 
         if self.transforms:
             im = self.transforms(im)
-        assert age >= 14 and age <= 62, "age=%d" % age
-        age = age - 14
 
         return im, age
 
     def __len__(self):
         return len(self.items)
+
+class IMDBDataset(torch.utils.data.Dataset):
+
+    def __init__(self, root, metadata, transforms=None):
+
+        self.root = root
+        self.transforms = transforms
+        assert isfile(metadata)
+        self.metadata = loadmat(metadata)["imdb"]
+
+        self.birth_day = self.metadata[0][0][0]
+        self.photo_taken_date = self.metadata[0][0][1]
+        self.items = self.metadata[0][0][2]
+        self.gender = self.metadata[0][0][3]
+
+        assert self.items.size == self.gender.size
+
+
+    def __getitem__(self, index):
+
+        filename = str(self.items[0, index][0])
+        im = pil_loader(join(self.root, filename))
+        birth_day = self.matlab_datenum_to_pydate(self.birth_day[0, index]).year
+        age = self.photo_taken_date[0, index] - birth_day
+
+        if self.transforms:
+            im = self.transforms(im)
+
+        return im, int(self.gender[0, index])
+
+    @staticmethod
+    def matlab_datenum_to_pydate(matlab_datenum):
+
+        matlab_datenum = int(matlab_datenum)
+
+        return datetime.fromordinal(int(matlab_datenum)) + timedelta(days=matlab_datenum%1) - timedelta(days = 366)
+
+    def __len__(self):
+
+        return self.items.size
+
+if __name__ == "__main__":
+
+    imdb = IMDBDataset("/media/data2/dataset/IMDB-face/imdb_crop", "/media/data2/dataset/IMDB-face/imdb/imdb.mat")
