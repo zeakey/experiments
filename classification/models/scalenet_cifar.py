9196704ce05b71cef2f182e7fff3cf8323b6db95
KAI ZHAO <kaiz.xyz@gmail.com>
2019-06-06 15:55:56 +0800

    allocate now migrates to BottleNeck

--- classification/models/scalenet_cifar.py
+++ classification/models/scalenet_cifar.py
@@ -38,88 +38,9 @@ class MSConv(nn.Module):
         y1 = self.conv1(x1)        
         y1 = torch.nn.functional.interpolate(y1, size=[h, w], mode="bilinear")
 
-        return torch.cat((y0, y1), dim=1)
-    
-    def allocate(self):
+        y = self.bn(torch.cat((y0, y1), dim=1))
 
-        temperature0 = self.bn.weight.data[:self.ch0]
-        temperature1 = self.bn.weight.data[self.ch0::]
-        
-        temp_diff = (temperature0.mean() - temperature1.mean()).item()
-
-        ch0_old = self.ch0
-        ch1_old = self.ch1
-
-        ch_transfer = abs(int(temp_diff * self.capacity * self.ch1))
-
-        if ch_transfer == 0:
-            ch_transfer = 1
-
-        if temp_diff > 0:
-            # conv0 is more important than conv1
-            # conv1 -> conv0
-
-            if self.ch1 - ch_transfer <= 0:
-                return temperature0, temperature1
-
-            ch0_old = self.ch0
-            ch1_old = self.ch1
-
-            self.ch0 = self.ch0 + ch_transfer
-            self.ch1 = self.ch1 - ch_transfer
-
-            v1, idx_retained1 = torch.sort(temperature1, descending=True)
-            idx_retained1 = idx_retained1[:self.ch1]
-
-            _, inplanes, kernel_size, _ = self.conv0.weight.data.shape
-            padding = self.conv0.padding
-            stride = self.conv0.stride
-
-            conv1w_retained = self.conv1.weight.data[idx_retained1, :, :, :]
-            self.conv1 = nn.Conv2d(inplanes, self.ch1, kernel_size=kernel_size,
-                              stride=stride, padding=padding, bias=False).cuda()
-            self.conv1.weight.data = conv1w_retained
-
-            conv0w_old = self.conv0.weight.data
-            self.conv0 = nn.Conv2d(inplanes, self.ch0, kernel_size=kernel_size,
-                              stride=stride, padding=padding, bias=False).cuda()
-            conv0w = torch.zeros(ch_transfer, inplanes, kernel_size, kernel_size).cuda()
-            nn.init.kaiming_normal_(conv0w, mode='fan_out', nonlinearity='relu')
-            self.conv0.weight.data = torch.cat((conv0w_old, conv0w), dim=0)
-
-        elif temp_diff < 0:
-            # conv1 is more important than conv0
-            # conv0 -> conv1
-
-            if self.ch0 - ch_transfer <= 0:
-                return temperature0, temperature1
-
-            ch0_old = self.ch0
-            ch1_old = self.ch1
-
-            self.ch0 = self.ch0 - ch_transfer
-            self.ch1 = self.ch1 + ch_transfer
-
-            v0, idx_retained0 = torch.sort(temperature0, descending=True)
-            idx_retained0 = idx_retained0[:self.ch0]
-
-            _, inplanes, kernel_size, _ = self.conv0.weight.data.shape
-            padding = self.conv0.padding
-            stride = self.conv0.stride
-
-            conv0w_retained = self.conv0.weight.data[idx_retained0, :, :, :]
-            self.conv0 = nn.Conv2d(inplanes, self.ch0, kernel_size=kernel_size,
-                              stride=stride, padding=padding, bias=False).cuda()
-            self.conv0.weight.data = conv0w_retained
-
-            conv1w_old = self.conv1.weight.data
-            self.conv1 = nn.Conv2d(inplanes, self.ch1, kernel_size=kernel_size,
-                              stride=stride, padding=padding, bias=False).cuda()
-            conv1w = torch.zeros(ch_transfer, inplanes, kernel_size, kernel_size).cuda()
-            torch.nn.init.kaiming_normal_(conv1w, mode='fan_out', nonlinearity='relu')
-            self.conv1.weight.data = torch.cat((conv1w_old, conv1w), dim=0)
-
-        return temperature0, temperature1
+        return y
 
 
 def conv3x3(in_planes, out_planes, stride=1):
@@ -214,6 +135,122 @@ class Bottleneck(nn.Module):
 
         return out
 
+    def allocate(self):
+        temperature = self.conv2.bn.weight.data.clone()
+        temperature0 = temperature[:self.conv2.ch0]
+        temperature1 = temperature[self.conv2.ch0::]
+
+        temp_diff = (temperature0.mean() - temperature1.mean()).item()
+
+        ch0_old = self.conv2.ch0
+        ch1_old = self.conv2.ch1
+
+        ch_transfer = abs(int(temp_diff * self.conv2.capacity * self.conv2.ch1))
+
+        if ch_transfer == 0:
+            ch_transfer = 1
+
+        if temp_diff > 0:
+            # conv0 is more important than conv1
+            # conv1 -> conv0
+
+            if self.conv2.ch1 - ch_transfer <= 0:
+                return temperature0, temperature1
+
+            conv_weight_old = torch.cat((self.conv2.conv0.weight.data,
+                                         self.conv2.conv1.weight.data), dim=0)
+
+            v1, idx1_sorted = torch.sort(temperature1, descending=True)
+            idx1_sorted = (idx1_sorted + ch0_old).tolist()
+            idx1_removed = idx1_sorted[-ch_transfer::]
+
+            order0 = list(range(ch0_old))
+            order1 = list(range(ch0_old, ch0_old+ch1_old))
+
+            for i in idx1_removed:
+                order0.append(i)
+                order1.remove(i)
+
+            order_new = order0 + order1
+
+            conv_weight_new = conv_weight_old[order_new]
+
+            _, inplanes, kernel_size, _ = self.conv2.conv0.weight.data.shape
+            padding = self.conv2.conv0.padding
+            stride = self.conv2.conv0.stride
+
+            self.conv2.conv0 = nn.Conv2d(inplanes, ch0_old+ch_transfer, kernel_size=kernel_size,
+                              stride=stride, padding=padding, bias=False).cuda()
+            self.conv2.conv0.weight.data = conv_weight_new[:ch0_old+ch_transfer, :, :, :]
+
+            self.conv2.conv1 = nn.Conv2d(inplanes, ch1_old-ch_transfer, kernel_size=kernel_size,
+                              stride=stride, padding=padding, bias=False).cuda()
+            self.conv2.conv1.weight.data = conv_weight_new[ch0_old+ch_transfer::, :, :, :]
+
+
+            #################################################
+            # NOTE: adjust other parameters order accordingly
+            #################################################
+            #self.conv2.bn.weight.data = self.conv2.bn.weight.data[order_new]
+            #self.conv2.bn.bias.data = self.conv2.bn.bias.data[order_new]
+
+            self.conv3.weight.data = self.conv3.weight.data[:, order_new, :, :]
+
+            # update ch0 and ch1 at last
+            self.conv2.ch0 = self.conv2.ch0 + ch_transfer
+            self.conv2.ch1 = self.conv2.ch1 - ch_transfer
+
+        elif temp_diff < 0:
+            # conv1 is more important than conv0
+            # conv0 -> conv1
+
+            if self.conv2.ch0 - ch_transfer <= 0:
+                return temperature0, temperature1
+
+            conv_weight_old = torch.cat((self.conv2.conv0.weight.data,
+                                         self.conv2.conv1.weight.data), dim=0)
+
+            v0, idx0_sorted = torch.sort(temperature0, descending=True)
+            idx0_sorted = idx0_sorted.tolist()
+            idx0_removed = idx0_sorted[-ch_transfer::]
+
+            order0 = list(range(ch0_old))
+            order1 = list(range(ch0_old, ch0_old+ch1_old))
+
+            for i in idx0_removed:
+                order0.remove(i)
+                order1.append(i)
+
+            order_new = order0 + order1
+
+            conv_weight_new = conv_weight_old[order_new]
+
+            _, inplanes, kernel_size, _ = self.conv2.conv0.weight.data.shape
+            padding = self.conv2.conv0.padding
+            stride = self.conv2.conv0.stride
+
+            self.conv2.conv0 = nn.Conv2d(inplanes, ch0_old-ch_transfer, kernel_size=kernel_size,
+                              stride=stride, padding=padding, bias=False).cuda()
+            self.conv2.conv0.weight.data = conv_weight_new[:ch0_old-ch_transfer, :, :, :]
+
+            self.conv2.conv1 = nn.Conv2d(inplanes, ch1_old+ch_transfer, kernel_size=kernel_size,
+                              stride=stride, padding=padding, bias=False).cuda()
+            self.conv2.conv1.weight.data = conv_weight_new[ch0_old-ch_transfer::, :, :, :]
+
+            #################################################
+            # NOTE: adjust other parameters order accordingly
+            #################################################
+            #self.conv2.bn.weight.data = self.conv2.bn.weight.data[order_new]
+            #self.conv2.bn.bias.data = self.conv2.bn.bias.data[order_new]
+
+            self.conv3.weight.data = self.conv3.weight.data[:, order_new, :, :]
+
+            # update ch0 and ch1 at last
+            self.conv2.ch0 = self.conv2.ch0 - ch_transfer
+            self.conv2.ch1 = self.conv2.ch1 + ch_transfer
+
+        return temperature0, temperature1
+
 
 class ResNet(nn.Module):
 
@@ -235,8 +272,8 @@ class ResNet(nn.Module):
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
             elif isinstance(m, nn.BatchNorm2d):
-                # nn.init.constant_(m.weight, 1)
-                nn.init.uniform_(m.weight, a=0, b=1)
+                nn.init.constant_(m.weight, 1)
+                # nn.init.uniform_(m.weight, a=0, b=1)
                 nn.init.constant_(m.bias, 0)
 
         # Zero-initialize the last BN in each residual branch,
@@ -283,8 +320,16 @@ class ResNet(nn.Module):
     
     def allocate(self):
         temperature = {}
+        # named_modules = list(self.named_modules())
+        # for i in range(len(named_modules)):
+        #     name, m = named_modules[i]
+        #     if isinstance(m, MSConv):
+        #         temperature0, temperature1 = m.allocate(named_modules[i+1][1])
+        #         temperature[name+'-0'] = temperature0
+        #         temperature[name+'-1'] = temperature1
+
         for name, m in self.named_modules():
-            if isinstance(m, MSConv):
+            if isinstance(m, Bottleneck):
                 temperature0, temperature1 = m.allocate()
                 temperature[name+'-0'] = temperature0
                 temperature[name+'-1'] = temperature1
