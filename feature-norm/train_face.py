d6f7d2d43e372d376d66a2e8e85fcf27d9b1cf0b
KAI ZHAO <kaiz.xyz@gmail.com>
2019-12-01 21:19:54 +0800

    update

--- feature-norm/train_face.py
+++ feature-norm/train_face.py
@@ -278,7 +278,7 @@ def train(train_loader, model, optimizer, lrscheduler, epoch):
         # measure data loading time
         data_time.update(time.time() - end)
 
-        output, feature = model(data)
+        output, feature = model(data, label=target)
         loss = criterion(output, target)
 
         acc1, acc5 = accuracy(output, target, topk=(1, 5))
