e5c90fc5d1e862e1add502f3d476c59ff4e86456
KAI ZHAO <kaiz.xyz@gmail.com>
2019-12-20 19:09:31 +0800

    sphereface now seems work

--- feature-norm/models/sphere_face.py
+++ feature-norm/models/sphere_face.py
@@ -85,7 +85,7 @@ class Sphere20(nn.Module):
         x = x + self.relu4_3(self.conv4_3(self.relu4_2(self.conv4_2(x))))
 
         x = x.view(x.size(0),-1)
-        
+
         x = self.fc5(x)
 
         return x
@@ -96,40 +96,89 @@ class AngleLinear(nn.Module):
         self.in_features = in_features
         self.out_features = out_features
         self.weight = Parameter(torch.zeros(out_features, in_features))
-        nn.init.normal_(self.weight, std=0.01)
+        nn.init.kaiming_uniform_(self.weight, 1.0)
         self.m = m
+        self.psi = Psi.apply
 
-        self.mlambda = [
-            lambda x: x**0,
-            lambda x: x**1,
-            lambda x: 2*x**2-1,
-            lambda x: 4*x**3-3*x,
-            lambda x: 8*x**4-8*x**2+1,
-            lambda x: 16*x**5-20*x**3+5*x
-        ]
+    def forward(self, input, label, lam=0):
 
-    def forward(self, input, label=None, lambd=0):
+        self.weight.data = F.normalize(self.weight.data, p=2, dim=1)
+        output = self.psi(input, self.weight, label, self.m, lam)
+        return output
 
-        input_norm = torch.norm(input, p=2, dim=1)
-        if label is None:
-            return F.linear(input, self.weight)
+mlambda = [
+    lambda x: x ** 0,
+    lambda x: x,
+    lambda x: 2 * x ** 2 - 1,
+    lambda x: 4 * x ** 3 - 3 * x,
+    lambda x: 8 * x ** 4 - 8 * x ** 2 + 1,
+    lambda x: 16 * x ** 5 - 20 * x ** 3 + 5 * x
+]
+mcoeff_w = [
+    lambda x: 0,
+    lambda x: 1,
+    lambda x: 4 * x,
+    lambda x: 12 * x ** 2 - 3,
+    lambda x: 32 * x ** 3 - 16 * x,
+    lambda x: 80 * x ** 4 - 60 * x ** 2 + 5
+]
+mcoeff_x = [
+    lambda x: -1,
+    lambda x: 0,
+    lambda x: 2 * x ** 2 + 1,
+    lambda x: 8 * x ** 3,
+    lambda x: 24 * x ** 4 - 8 * x ** 2 - 1,
+    lambda x: 64 * x ** 5 - 40 * x ** 3
+]
 
-        normed_weight = F.normalize(self.weight, p=2, dim=1)
-        normed_input = F.normalize(input, p=2, dim=1)
+class Psi(torch.autograd.Function):
 
-        cos_theta = F.linear(normed_input, normed_weight)
+    @staticmethod
+    def forward(ctx, input, weight, label, m, lam):
 
-        cos_m_theta = self.mlambda[self.m](cos_theta)
-        theta = cos_theta.data.acos()
-        k = (self.m*theta/math.pi).floor()
-        phi_theta = (-1)**k * cos_m_theta - 2*k
+        xlen = torch.norm(input, p=2, dim=1).view(-1, 1)
+        wn = F.normalize(weight, p=2, dim=1)
+        xn = F.normalize(input, p=2, dim=1)
+        cos_theta = xn.mm(wn.t())
+        cos_m_theta = mlambda[m](cos_theta)
 
-        # NOTE that sphereface doesn't normalize features
-        cos_theta = cos_theta * input_norm.view(-1, 1)
-        phi_theta = phi_theta * input_norm.view(-1, 1)
+        k = (m * cos_theta.acos() / math.pi).floor()
+        psi_theta = (-1) ** k * cos_m_theta - 2 * k
 
-        one_hot = torch.zeros_like(cos_theta, dtype=bool)
-        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
-        output = torch.where(one_hot, lambd*phi_theta+(1-lambd)*cos_theta, cos_theta)
+        onehot = torch.zeros_like(cos_theta, dtype=bool)
+        onehot = onehot.scatter_(1, label.view(-1, 1), 1)
+
+        output = torch.where(onehot,
+                             psi_theta * lam + cos_theta * (1 - lam),
+                             cos_theta)
+        output = output * xlen
+
+        # save for backward
+        cos_theta = (cos_theta * onehot.to(input.dtype)).sum(dim=1).view(-1, 1)
+        k = (k * onehot.to(input.dtype)).sum(dim=1).view(-1, 1)
+        lam = torch.tensor(lam, device=input.device)
+        m = torch.tensor(m, device=input.device)
+        ctx.save_for_backward(input, label, weight , cos_theta, k, m, lam)
 
         return output
+
+    @staticmethod
+    def backward(ctx, grad_outputs):
+
+        input, label, weight , cos_theta, k, m, lam = ctx.saved_tensors
+        xlen = torch.norm(input, p=2, dim=1).view(-1, 1)
+        onehot = torch.zeros_like(grad_outputs, dtype=bool)
+        onehot = onehot.scatter_(1, label.view(-1, 1), 1)
+        m = m.item()
+
+        grad_input = grad_outputs.mm(weight) * (1-lam)
+        grad_outputs_label = (grad_outputs*onehot.to(grad_outputs.dtype)).sum(dim=1).view(-1, 1)
+        coeff_w = ((-1) ** k) * mcoeff_w[m](cos_theta)
+        coeff_x = (((-1) ** k) * mcoeff_x[m](cos_theta) + 2 * k) / xlen
+        coeff_norm = (coeff_w.pow(2) + coeff_x.pow(2)).pow(0.5)
+        grad_input += grad_outputs_label * (coeff_w / coeff_norm) * torch.index_select(weight, 0, label) * lam
+        grad_input -= grad_outputs_label * (coeff_x / coeff_norm) * input * lam
+        grad_input += (grad_outputs * (1 - onehot.to(grad_outputs.dtype))).mm(weight) * lam
+        grad_weight = grad_outputs.t().mm(input)
+
+        return grad_input, grad_weight, None, None, None
