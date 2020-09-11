2225eb4b2ab974c82a5eec3ccc0264e944d61105
KAI ZHAO <6763078+zeakey@users.noreply.github.com>
2020-09-11 23:17:59 +0800

    update

--- sod/models/backbone/resnet.py
+++ sod/models/backbone/resnet.py
@@ -123,7 +123,7 @@ class Bottleneck(nn.Module):
 
 class ResNet(nn.Module):
 
-    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
+    def __init__(self, block, layers, num_classes=1000, zero_init_residual=True,
                  groups=1, width_per_group=64, replace_stride_with_dilation=None,
                  norm_layer=None, return_middle=False):
         super(ResNet, self).__init__()
@@ -155,8 +155,6 @@ class ResNet(nn.Module):
                                        dilate=replace_stride_with_dilation[1])
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
                                        dilate=replace_stride_with_dilation[2])
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
