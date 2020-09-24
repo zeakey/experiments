2ec3e61cdd97af5ad87e7dd41a9434a4b05d12c8
KAI ZHAO <6763078+zeakey@users.noreply.github.com>
2020-09-25 01:40:40 +0800

    cv2 dataset

--- sod/dataset.py
+++ sod/dataset.py
@@ -5,6 +5,7 @@ from scipy.ndimage.morphology import distance_transform_edt
 from os.path import join, isdir, isfile
 from skimage.filters import sobel_h, sobel_v
 import cv2
+import random
 from vlkit.dense import seg2edge, dense2flux, flux2angle, quantize_angle, dequantize_angle
 
 def edge2flux(edge):
@@ -31,16 +32,18 @@ def edge2flux(edge):
 
 
 class SODDataset(torch.utils.data.Dataset):
-    def __init__(self, img_dir, label_dir, name_list, flip=True, image_transform=None, label_transform=None):
+    def __init__(self, img_dir, label_dir, name_list, flip=True, imsize=[0,0], image_transform=None, label_transform=None):
         assert isdir(img_dir), "%s doesn't exist" % img_dir
         assert isdir(label_dir), "%s doesn't exist" % label_dir
         assert isfile(name_list)
+        assert isinstance(imsize, list) and len(imsize) == 2
 
         self.img_dir = img_dir
         self.label_dir = label_dir
         self.flip = flip
         self.image_transform = image_transform
         self.label_transform = label_transform
+        self.imsize = np.array(imsize)
         self.__item_names = [line.strip() for line in open(name_list, 'r')]
 
         assert isdir(img_dir) and isdir(label_dir)
@@ -54,30 +57,36 @@ class SODDataset(torch.utils.data.Dataset):
         assert isfile(lb_fullname), lb_fullname
         label = Image.open(lb_fullname).convert("L")
 
-        flip = self.flip and np.random.rand() > 0.5
+        if image.size != label.size:
+            label = label.resize((image.width, image.height))
+
+        flip = self.flip and np.random.choice([True, False])
         if flip:
             image = image.transpose(Image.FLIP_LEFT_RIGHT)
             label = label.transpose(Image.FLIP_LEFT_RIGHT)
+        if np.all(self.imsize > 0):
+            H, W = self.imsize
+            image = image.resize((W, H))
+            label = label.resize((W, H))
+        image = np.array(image, dtype=np.uint8)
+        label = np.array(label, dtype=np.uint8)
 
-        if self.image_transform:
-            image = self.image_transform(image)
-        if self.label_transform:
-            label = self.label_transform(label)
-
-        label = np.array(label).astype(bool)
-        
-        edge = seg2edge(label)
+        edge = seg2edge(label.astype(bool))
         dist = distance_transform_edt(np.logical_not(edge))
         mask = dist <= 15
         mask[dist <= 1] = False
 
         angle = flux2angle(dense2flux(dist))
-        orientation = quantize_angle(angle, num_bins=8)
+        orientation = quantize_angle(angle, num_bins=8).astype(np.int64)
 
-        label = np.expand_dims(label, axis=0)
-        edge = np.expand_dims(edge, axis=0)
-        # orientation = np.expand_dims(orientation, axis=0)
-        # mask = np.expand_dims(mask, axis=0)
+        if self.image_transform:
+            image = self.image_transform(image)
+        if self.label_transform:
+            label = self.label_transform(label)
+            edge = self.label_transform(edge)
+            orientation = self.label_transform(orientation)
+            mask = self.label_transform(mask)
+
         metas = dict({
             "filename": img_fullname,
             "flip": flip
@@ -88,6 +97,69 @@ class SODDataset(torch.utils.data.Dataset):
             "edge": edge,
             "orientation": orientation,
             "mask": mask,
+            "metas": metas
+        })
+
+        return result
+
+    def __len__(self):
+        return len(self.__item_names)
+
+    def get_item_names(self):
+        return self.__item_names.copy()
+
+class PoolNetDataset(torch.utils.data.Dataset):
+    def __init__(self, img_dir, label_dir, name_list, flip=True, imsize=[0,0]):
+        assert isdir(img_dir), "%s doesn't exist" % img_dir
+        assert isdir(label_dir), "%s doesn't exist" % label_dir
+        assert isfile(name_list)
+        assert isinstance(imsize, list) and len(imsize) == 2
+
+        self.img_dir = img_dir
+        self.label_dir = label_dir
+        self.flip = flip
+        self.imsize = np.array(imsize)
+        self.__item_names = [line.strip() for line in open(name_list, 'r')]
+
+        assert isdir(img_dir) and isdir(label_dir)
+
+    def __getitem__(self, index):        
+        img_fullname = join(self.img_dir, self.__item_names[index]+".jpg")
+        assert isfile(img_fullname), img_fullname
+        image = cv2.imread(img_fullname, 1)
+        assert image.ndim == 3
+        image = image - np.array((104.00699, 116.66877, 122.67892))
+
+        lb_fullname = join(self.label_dir, self.__item_names[index]+".png")
+        assert isfile(lb_fullname), lb_fullname
+        label = cv2.imread(lb_fullname, 0) / 255.0
+        assert label.ndim == 2
+
+        if image.shape[:2] != label.shape:
+            print("image and label shape mismatch, reshape.")
+            H, W = image.shape[:2]
+            label = cv2.resize(label, (W, H))
+
+        flip = self.flip and np.random.choice([True, False])
+        if flip:
+            image = cv2.flip(image, 1)
+            label = cv2.flip(label, 1)
+
+        if np.all(self.imsize > 0):
+            H, W = self.imsize
+            image = cv2.resize(image, (W, H))
+            label = cv2.resize(label, (W, H))
+
+        image = image.transpose((2,0,1))
+        label = label[np.newaxis, :, :]
+        
+        metas = dict({
+            "filename": img_fullname,
+            "flip": flip
+        })
+        result = dict({
+            "image": image.astype(np.float32),
+            "label": label.astype(np.float32),
             "metas": metas
         })
 
