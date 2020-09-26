08bd787620c7c924db30c51a84b6de48de0c3a75
KAI ZHAO <6763078+zeakey@users.noreply.github.com>
2020-09-26 12:12:10 +0800

    unify dataset

--- sod/dataset.py
+++ sod/dataset.py
@@ -32,7 +32,8 @@ def edge2flux(edge):
 
 
 class SODDataset(torch.utils.data.Dataset):
-    def __init__(self, img_dir, label_dir, name_list, flip=True, imsize=[0,0], image_transform=None, label_transform=None):
+    def __init__(self, img_dir, label_dir, name_list, flip=True, imsize=[0,0], backend="pil",
+                 image_transform=None, label_transform=None):
         assert isdir(img_dir), "%s doesn't exist" % img_dir
         assert isdir(label_dir), "%s doesn't exist" % label_dir
         assert isfile(name_list)
@@ -41,9 +42,11 @@ class SODDataset(torch.utils.data.Dataset):
         self.img_dir = img_dir
         self.label_dir = label_dir
         self.flip = flip
+        self.imsize = np.array(imsize)
+        self.backend = backend
         self.image_transform = image_transform
         self.label_transform = label_transform
-        self.imsize = np.array(imsize)
+
         self.__item_names = [line.strip() for line in open(name_list, 'r')]
 
         assert isdir(img_dir) and isdir(label_dir)
@@ -51,11 +54,11 @@ class SODDataset(torch.utils.data.Dataset):
     def __getitem__(self, index):        
         img_fullname = join(self.img_dir, self.__item_names[index]+".jpg")
         assert isfile(img_fullname), img_fullname
-        image = imread(img_fullname, backend="pil")
+        image = imread(img_fullname, backend=self.backend)
 
         lb_fullname = join(self.label_dir, self.__item_names[index]+".png")
         assert isfile(lb_fullname), lb_fullname
-        label = imread(lb_fullname, backend="pil", grayscale=True)
+        label = imread(lb_fullname, backend=self.backend, grayscale=True)/255.0
 
         if image.shape[:2] != label.shape:
             print("image and label shape mismatch, reshape.")
@@ -69,10 +72,8 @@ class SODDataset(torch.utils.data.Dataset):
 
         if np.all(self.imsize > 0):
             H, W = self.imsize
-            image = image.resize((W, H))
-            label = label.resize((W, H))
-        image = np.array(image, dtype=np.uint8)
-        label = np.array(label, dtype=np.uint8)
+            image = cv2.resize(image, (W, H))
+            label = cv2.resize(label, (W, H))
 
         edge = seg2edge(label.astype(bool))
         dist = distance_transform_edt(np.logical_not(edge))
@@ -100,72 +101,6 @@ class SODDataset(torch.utils.data.Dataset):
             "edge": edge,
             "orientation": orientation,
             "mask": mask,
-            "metas": metas
-        })
-
-        return result
-
-    def __len__(self):
-        return len(self.__item_names)
-
-    def get_item_names(self):
-        return self.__item_names.copy()
-
-# Caffe style data loading code
-# image encode/decode with cv2
-# images are not normalized (/255)
-class CaffeDataset(torch.utils.data.Dataset):
-    def __init__(self, img_dir, label_dir, name_list, flip=True, imsize=[0,0]):
-        assert isdir(img_dir), "%s doesn't exist" % img_dir
-        assert isdir(label_dir), "%s doesn't exist" % label_dir
-        assert isfile(name_list)
-        assert isinstance(imsize, list) and len(imsize) == 2
-
-        self.img_dir = img_dir
-        self.label_dir = label_dir
-        self.flip = flip
-        self.imsize = np.array(imsize)
-        self.__item_names = [line.strip() for line in open(name_list, 'r')]
-
-        assert isdir(img_dir) and isdir(label_dir)
-
-    def __getitem__(self, index):        
-        img_fullname = join(self.img_dir, self.__item_names[index]+".jpg")
-        assert isfile(img_fullname), img_fullname
-        image = imread(img_fullname, backend="cv2")
-        assert image.ndim == 3
-        image = image - np.array((104.00699, 116.66877, 122.67892))
-
-        lb_fullname = join(self.label_dir, self.__item_names[index]+".png")
-        assert isfile(lb_fullname), lb_fullname
-        label = imread(lb_fullname, backend="cv2", grayscale=True) / 255.0
-        assert label.ndim == 2
-
-        if image.shape[:2] != label.shape:
-            print("image and label shape mismatch, reshape.")
-            H, W = image.shape[:2]
-            label = cv2.resize(label, (W, H))
-
-        flip = self.flip and np.random.choice([True, False])
-        if flip:
-            image = cv2.flip(image, 1)
-            label = cv2.flip(label, 1)
-
-        if np.all(self.imsize > 0):
-            H, W = self.imsize
-            image = cv2.resize(image, (W, H))
-            label = cv2.resize(label, (W, H))
-
-        image = image.transpose((2,0,1))
-        label = label[np.newaxis, :, :]
-        
-        metas = dict({
-            "filename": img_fullname,
-            "flip": flip
-        })
-        result = dict({
-            "image": image.astype(np.float32),
-            "label": label.astype(np.float32),
             "metas": metas
         })
 
