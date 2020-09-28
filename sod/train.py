6c0b06ff9e91d647fb9e3718f27fc9e9ace023ab
KAI ZHAO <6763078+zeakey@users.noreply.github.com>
2020-09-28 21:37:20 +0800

    update

--- sod/train.py
+++ sod/train.py
@@ -10,7 +10,7 @@ import os, sys, argparse, time, shutil
 from os.path import join, split, isdir, isfile, dirname, abspath, split, splitext
 from torch.utils.tensorboard import SummaryWriter
 from PIL import Image
-from vlkit.training import get_logger
+from vlkit import get_logger
 from vlkit import image as vlimage
 from vlkit.pytorch import save_checkpoint, AverageMeter
 from vlkit.lr import CosAnnealingLR, MultiStepLR
@@ -20,50 +20,48 @@ import apex
 from apex.parallel import DistributedDataParallel as DDP
 import warnings
 warnings.filterwarnings("ignore", "(Possibly )?corrupt EXIF data", UserWarning)
-from drn_seg import DRNSeg
+from models.drn_seg import DRNSeg
+from models.poolnet import poolnet
 from dataset import SODDataset
 from utils import accuracy, init_from_pretrained, save_maps
 from crf import par_batch_crf_dataloader
 from floss import FLoss, F_cont
-
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
 parser.add_argument('--print-freq', default=5, type=int,
                     metavar='N', help='print frequency (default: 10)')
-parser.add_argument('--arch', '--a', metavar='STR', default="drn_d_38", help='model')
+parser.add_argument('--arch', '--a', metavar='STR', default="drn_d_54", help='model')
 # data
 parser.add_argument('--train-data', metavar='DIR', default="data/DUTS/DUTS-TR", help='path to dataset')
 parser.add_argument('--test-data', metavar='DIR', default="data/ECSSD", help='path to dataset')
+parser.add_argument('--backend', default="cv2", help='Image decode backend (cv2 | pil)')
 parser.add_argument('--use-rec', action='store_true', help='Use mxnet record.')
-parser.add_argument('--batch-size', default=20, type=int, metavar='N', help='mini-batch size')
-parser.add_argument('--imsize', default=432, type=int, metavar='N', help='im crop size')
+parser.add_argument('--batch-size', default=8, type=int, metavar='N', help='mini-batch size')
+parser.add_argument('--imsize', default="320, 320", type=str, help='image size')
 parser.add_argument('-j', '--workers', default=1, type=int, help='number of data loading workers')
 # optimization
-parser.add_argument('--epochs', default=20, type=int, metavar='N',
+parser.add_argument('--epochs', default=24, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--warmup-epochs', type=int, default=2, help="warmup epochs")
-parser.add_argument('--milestones', default="10,15", type=str)
+parser.add_argument('--milestones', default="12,18", type=str)
 parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
                     metavar='LR', help='initial learning rate')
-parser.add_argument('--stepsize', '--step-size', default=None, type=int,
-                    metavar='SS', help='decrease learning rate every stepsize epochs')
-parser.add_argument('--gamma', default=0.1, type=float,
-                    metavar='GM', help='decrease learning rate by gamma')
+parser.add_argument('--lr-scheduler', default="cos", type=str, help='LR scheduler')
+parser.add_argument('--gamma', default=0.1, type=float, help='decrease learning rate by gamma')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='momentum')
-parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
-                    metavar='W', help='weight decay')
+parser.add_argument('--weight-decay', '--wd', default=5e-5, type=float, help='weight decay')
+parser.add_argument('--freeze-bn', action='store_true', help='Freeze BN parameters')
 parser.add_argument('--resume', default="", type=str, metavar='PATH',
                     help='path to latest checkpoint')
 parser.add_argument('--tmp', help='tmp folder', default="tmp")
-parser.add_argument('--pretrained', help='pretrained model', type=str, default="Pretrained_Models/drn_pretraining/drn_d_38_cityscapes.pth")
+parser.add_argument('--pretrained', help='pretrained model', type=str, default="drn_d_54-0e0534ff.pth")
 # Loss function
 parser.add_argument('--loss', help='loss function', type=str, default="ce")
-parser.add_argument('--floss-beta', help='floss beta', type=float, default=1)
+parser.add_argument('--reduction', help='loss reduction', type=str, default="mean")
 # FP16
-parser.add_argument('--fp16', action='store_true',
-                    help='half precision training.')
+parser.add_argument('--fp16', action='store_true', help='half precision training.')
 parser.add_argument('--static-loss-scale', type=float, default=128,
                     help='Static loss scale, positive power of 2 values can improve fp16 convergence.')
 parser.add_argument('--dynamic-loss-scale', action='store_true',
@@ -83,6 +81,7 @@ if args.local_rank == 0:
     os.makedirs(args.tmp, exist_ok=True)
 
 args.milestones = [int(i) for i in args.milestones.split(',')]
+args.imsize = [int(i) for i in args.imsize.split(',')]
 
 torch.backends.cudnn.benchmark = True
 
@@ -108,24 +107,23 @@ if args.fp16:
     assert args.distributed, "FP16 can be only used in Distributed mode"
     assert torch.backends.cudnn.enabled, "fp16 mode requires cudnn backend to be enabled."
 
-
-
-floss = FLoss(beta=args.floss_beta)
-CELoss = nn.CrossEntropyLoss()
-
-
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                        std=[0.229, 0.224, 0.225])
-image_transform = transforms.Compose([transforms.Resize([args.imsize]*2), transforms.ToTensor(), normalize])
-label_transform = transforms.Compose([transforms.Resize([args.imsize]*2, interpolation=Image.NEAREST)])
+def image_transform(image):
+    assert isinstance(image, np.ndarray) and image.ndim == 3, "%s, %s"%(type(image), image.shape)
+    image = image - np.array((104.00699, 116.66877, 122.67892))
+    image = image / 255.0
+    return image.astype(np.float32).transpose((2,0,1))
+def label_transform(label):
+    assert isinstance(label, np.ndarray) and label.ndim == 2
+    label = label[np.newaxis, :, :]
+    return label
 train_dataset = SODDataset(img_dir=join(args.train_data, "images"),
-                label_dir=join(args.train_data, "gt-masks"),
-                name_list=join(args.train_data, "train_names.txt"),
-                flip=True, image_transform=image_transform, label_transform=label_transform)
+                label_dir=join(args.train_data, "gt-masks"), imsize=args.imsize,
+                name_list=join(args.train_data, "train_names.txt"), flip=True, backend=args.backend,
+                image_transform=image_transform, label_transform=label_transform)
 test_dataset = SODDataset(img_dir=join(args.test_data, "images/"),
-                label_dir=join(args.test_data, "gt-masks/"),
-                name_list=join(args.test_data, "test_names.txt"),
-                flip=False, image_transform=image_transform, label_transform=label_transform)
+                label_dir=join(args.test_data, "gt-masks/"), imsize=args.imsize,
+                name_list=join(args.test_data, "test_names.txt"), flip=False, backend=args.backend,
+                image_transform=image_transform, label_transform=label_transform)
 
 if args.distributed:
         train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
@@ -147,25 +145,17 @@ test_loader = torch.utils.data.DataLoader(
 
 train_loader_len = len(train_loader)
 
-
-def l2loss(x, y, mask=None):
-    assert x.shape == y.shape
-
-    loss = (x - y)**2
-    loss = loss.sum(dim=1).sqrt().unsqueeze(dim=1)
-    if mask is not None:
-        loss = (loss * mask).sum() / mask.sum()
-    else:
-        loss = loss.mean()
-    return loss
-
-
-
 def main():
     if args.local_rank == 0:
         logger.info(args)
     # model and optimizer
-    model = DRNSeg(args.arch, setting=args.setting, classes=1)
+    model = DRNSeg("drn_d_54", setting=args.setting, classes=1)
+    if args.freeze_bn:
+        for name, p in model.named_parameters():
+            if "base" in name and "bn" in name:
+                p.requires_grad = False
+                if args.local_rank == 0:
+                    logger.info("Set requires_grad=False: %s"%name)
 
     if isfile(args.pretrained):
         if args.local_rank == 0:
@@ -179,20 +169,22 @@ def main():
         model = apex.parallel.convert_syncbn_model(model)
 
     model = model.cuda()
-    optimizer = torch.optim.SGD(
-        model.parameters(),
-        lr=args.lr,
-        momentum=args.momentum,
-        weight_decay=args.weight_decay
-    )
+
+    
+    if True:
+        # poolnet optimizer
+        optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr, weight_decay=args.weight_decay)
+        lr_scheduler = None
+    else:
+        optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr,
+                                    weight_decay=args.weight_decay, momentum=args.momentum)
+        lr_scheduler = CosAnnealingLR(epoch_size=len(train_loader), epochs=args.epochs, max_lr=args.lr, min_lr=0, warmup_epochs=args.warmup_epochs)
 
     if args.fp16:
         model, optimizer = amp.initialize(model, optimizer, opt_level="O2")
 
     if args.distributed:
         model = DDP(model, delay_allreduce=True)
-    # else:
-    #     model = torch.nn.DataParallel(model)
 
     if args.local_rank == 0:
         logger.info(optimizer)
@@ -200,39 +192,23 @@ def main():
     # records
     best_mae = 10000
 
-    # optionally resume from a checkpoint
-    if args.resume:
-        if isfile(args.resume):
-            shutil.copy(args.resume, args.tmp)
-            if args.local_rank == 0:
-                logger.info("=> loading checkpoint '{}'".format(args.resume))
-            checkpoint = torch.load(args.resume, map_location=torch.device('cpu'))
-            args.start_epoch = checkpoint['epoch']
-            best_mae = checkpoint['best_mae']
-            model.load_state_dict(checkpoint['state_dict'])
-            optimizer.load_state_dict(checkpoint['optimizer'])
-            if args.local_rank == 0:
-                logger.info("=> loaded checkpoint '{}' (epoch {})"
-                    .format(args.resume, checkpoint['epoch']))
-        else:
-            if args.local_rank == 0:
-                logger.info("=> no checkpoint found at '{}'".format(args.resume))
-
-    lr_scheduler = CosAnnealingLR(loader_len=len(train_loader), epochs=args.epochs, max_lr=args.lr, min_lr=0, warmup_epochs=args.warmup_epochs)
-    # lr_scheduler = MultiStepLR(loader_len=len(train_loader), base_lr=args.lr, gamma=args.gamma, milestones=[15, 100], warmup_epochs=args.warmup_epochs)
-
     for epoch in range(args.epochs):
         train_loss, ori_losses = train_epoch(model, train_loader, optimizer, lr_scheduler, epoch)
-        test_mae, test_f, ori_acc = test(model, test_loader, epoch)
+        test_loss, test_mae, ori_acc = test(model, test_loader, epoch)
+
+        if epoch in args.milestones:
+            args.lr = args.lr * args.gamma
+            if args.local_rank == 0:
+                logger.info("Epoch %d, decay learning rate to %f"%(epoch, args.lr))
+            optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr, weight_decay=args.weight_decay)
 
         if args.local_rank == 0:
             tfboard_writer.add_scalar("train/seg-loss", train_loss, epoch)
             tfboard_writer.add_scalar("train/ori-loss", ori_losses, epoch)
             tfboard_writer.add_scalar("train/lr", optimizer.param_groups[0]["lr"], epoch)
+            tfboard_writer.add_scalar("test/seg-loss", test_loss, epoch)
             tfboard_writer.add_scalar("test/mae", test_mae, epoch)
-            tfboard_writer.add_scalar("test/F-measure", test_f, epoch)
             tfboard_writer.add_scalar("test/Orientation-Accuracy", ori_acc, epoch)
-
 
     if args.local_rank == 0:
         logger.info("Optimization done, ALL results saved to %s." % args.tmp)
@@ -249,31 +225,44 @@ def train_epoch(model, train_loader, optimizer, lr_scheduler, epoch):
 
     train_loader_len = len(train_loader)
     # switch to train mode
-    model.train()
+    if args.freeze_bn:
+        model.eval()
+    else:
+        model.train()
 
     end = time.time()
     for i, data in enumerate(train_loader):
 
         image = data["image"]
-        target = data["label"].long()
-        orientation = data["orientation"]
-        mask = data["mask"]
+        target = data["label"]
+        # edge = data["edge"]
+        # orientation = data["orientation"]
+        # mask = data["mask"]
         if args.distributed:
             image = image.cuda(non_blocking=True).to(dtype=DTYPE)
 
-        image = image.cuda(non_blocking=True)
-        target = target.cuda(non_blocking=True).float()
-        orientation = orientation.cuda(non_blocking=True).long()
-        mask = mask.cuda(non_blocking=True)
+        image = image.cuda() # (non_blocking=True)
+        target = target.cuda().float() # (non_blocking=True).float()
+        # edge = edge.cuda(non_blocking=True).float()
+        # orientation = orientation.cuda(non_blocking=True).long()
+        # mask = mask.cuda(non_blocking=True)
 
         # measure data loading time
         data_time.update(time.time() - end)
-        output = model(image)
+        output = model(image)["seg"]
 
-        seg_loss = F.binary_cross_entropy(torch.sigmoid(output["seg"]), target)
-        ori_loss = F.cross_entropy(output["orientation"], orientation, reduction="none")
-        ori_loss = (ori_loss * mask).sum() / mask.sum()
+        # seg_loss = F.binary_cross_entropy(torch.sigmoid(output["seg"]), target)
+        seg_loss = F.binary_cross_entropy_with_logits(output, target, reduction=args.reduction)
 
+        # ori_loss = F.cross_entropy(output["orientation"], orientation, reduction="none")
+        # ori_loss = (ori_loss * mask).sum() / mask.sum()
+        ori_loss = torch.zeros_like(seg_loss)
+
+        # edge_loss = F.binary_cross_entropy(torch.sigmoid(output["edge"]), edge, reduction="none")
+        # mask = torch.where(edge==0, edge.mean(), 1-edge.mean())
+        # mask /= max(mask.mean(), 1e-5)
+        # edge_loss = (edge_loss * mask).mean()
+        ori_loss = torch.zeros_like(seg_loss)
 
         if args.distributed:
             reduced_seg_loss = reduce_tensor(seg_loss.data)
@@ -292,7 +281,7 @@ def train_epoch(model, train_loader, optimizer, lr_scheduler, epoch):
                 param_group['lr'] = lr
 
         # compute gradient and do SGD step
-        loss = seg_loss + ori_loss
+        loss = seg_loss# + ori_loss
         optimizer.zero_grad()
 
         # scale loss before backward
@@ -322,19 +311,13 @@ def train_epoch(model, train_loader, optimizer, lr_scheduler, epoch):
                    epoch, args.epochs, i, train_loader_len,
                    batch_time=batch_time, data_time=data_time, seg_loss=seg_losses, ori_loss=ori_losses, lr=lr))
 
-        # if args.local_rank == 0 and i == 0:
-        #     data = torchvision.utils.make_grid(image, normalize=True)
-        #     pred = torchvision.utils.make_grid(torch.sigmoid(output), normalize=True)
-        #     # tfboard_writer.add_image("images", data, epoch)
-        #     # tfboard_writer.add_image("predictions", pred, epoch)
-
+            tfboard_writer.add_scalar("train/iter-segloss", seg_losses.val, epoch*len(train_loader)+i)
     return seg_losses.avg, ori_losses.avg
 
 def test(model, test_loader, epoch):
     batch_time = AverageMeter()
     seg_losses = AverageMeter()
     mae =  AverageMeter()
-    fmeasure = AverageMeter()
     ori_acc1 = AverageMeter()
     # switch to evaluate mode
     model.eval()
@@ -346,27 +329,27 @@ def test(model, test_loader, epoch):
             metas = data["metas"]
             image = data["image"].cuda()
             target = data["label"].cuda().float()
-            mask = data["mask"].cuda()
-            orientation = data["orientation"].cuda().long()
+            # mask = data["mask"].cuda()
+            # orientation = data["orientation"].cuda().long()
 
             # compute output
-            output = model(image)
-            seg_pred = torch.sigmoid(output["seg"])
-            seg_loss = torch.nn.functional.binary_cross_entropy(seg_pred, target)
+            output = model(image)["seg"]
+            seg_pred = torch.sigmoid(output)
+            seg_loss = torch.nn.functional.binary_cross_entropy_with_logits(output, target, reduction=args.reduction)
 
-            ori_pred = output["orientation"].transpose(0,1)[:, mask].t()
-            ori_gt = orientation[mask]
-            ori_top1, ori_top2 = accuracy(ori_pred, ori_gt, topk=(1,2))
+            # ori_pred = output["orientation"].transpose(0,1)[:, mask].t()
+            # ori_gt = orientation[mask]
+            # ori_top1, ori_top2 = accuracy(ori_pred, ori_gt, topk=(1,2))
+            ori_top1 = torch.zeros_like(seg_loss)
 
             # mae and F-measure
             mae_ = (seg_pred - target).abs().mean()
-            f_ = floss(seg_pred, target)
 
             filenames = [splitext(split(i)[-1])[0] for i in metas["filename"]]
             diff = ((seg_pred >= 0.5).float() - target).detach().cpu().numpy()
             diff = (diff * 127 + 128)
 
-            if (epoch+1) % 200 == 0 or epoch == args.epochs-1:
+            if (epoch+1) % 5 == 0 or epoch == args.epochs-1:
                 seg_pred = (seg_pred >= 0.5).cpu().numpy()*255
                 gt = target.cpu().numpy()*255
                 diff = np.concatenate((seg_pred, gt, diff), axis=3).astype(np.uint8)
@@ -375,15 +358,12 @@ def test(model, test_loader, epoch):
             if args.distributed:
                 reduced_seg_loss = reduce_tensor(seg_loss.data)
                 reduced_mae = reduce_tensor(mae_)
-                reduced_f = reduce_tensor(f_)
             else:
                 reduced_seg_loss = seg_loss.data
                 reduced_mae = mae_.data
-                reduced_f = f_.data
 
             seg_losses.update(reduced_seg_loss.item(), image.size(0))
             mae.update(reduced_mae.item(), image.size(0))
-            fmeasure.update(reduced_f.item(), image.size(0))
             ori_acc1.update(ori_top1.item(), image.size(0))
             # measure elapsed time
             batch_time.update(time.time() - end)
@@ -392,15 +372,15 @@ def test(model, test_loader, epoch):
             if args.local_rank == 0 and i % args.print_freq == 0:
                 logger.info('Test: [{0}/{1}]\t'
                       'SegLoss {seg_loss.val:.3f} (avg={seg_loss.avg:.3f})\t'
-                      'MAE {mae.val:.3f} (avg={mae.avg:.3f}) F {fmeasure.val:3f} (avg={fmeasure.avg:.3f})\t'
+                      'MAE {mae.val:.3f} (avg={mae.avg:.3f})\t'
                       'OriAcc={ori_acc.avg:.3f}\t'.format(
-                       i, test_loader_len, seg_loss=seg_losses, mae=mae, fmeasure=fmeasure, ori_acc=ori_acc1))
+                       i, test_loader_len, seg_loss=seg_losses, mae=mae, ori_acc=ori_acc1))
 
     if args.local_rank == 0:
-        logger.info(' * MAE {mae.avg:.5f} F-measure {fmeasure.avg:.5f} OriAcc={ori_acc.avg:.3f}'\
-            .format(mae=mae, fmeasure=fmeasure, ori_acc=ori_acc1))
+        logger.info(' * MAE {mae.avg:.5f} OriAcc={ori_acc.avg:.3f}'\
+            .format(mae=mae, ori_acc=ori_acc1))
 
-    return mae.avg, fmeasure.avg, ori_acc1.avg
+    return seg_losses.avg, mae.avg, ori_acc1.avg
 
 def reduce_tensor(tensor):
     rt = tensor.clone()
